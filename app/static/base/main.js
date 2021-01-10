e88986d408ef496eae7d5304376547abfee873ff
Vicente Lineros Pardo <vlinerospardo@gmail.com>
2021-01-10 15:47:21 -0300

    .

--- app/static/base/main.js
+++ app/static/base/main.js
@@ -1,16 +1,20 @@
-const url = "http://192.168.1.5"
+const url_arduino = "http://192.168.1.6"
+const url_host = "http://192.168.1.5"
 const beet_port = "9000"
 
 async function sendCommand(cmd){
-	return fetch(`${url}/${cmd}`, {method: 'POST'})
-	    .then( function(response) {
-	      if (response.status !== 200)  return false;
-	        response.text().then(function(data) {
-	        	console.log("main")
-	        	datas = data
-	          	return data
-	        }
-	      ).catch(err => console.log("ERROR"))
-	    })
-    .catch(err => false);
+
+  var formdata = new FormData();
+  formdata.append("command", cmd);
+
+  var requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+  };
+
+	return fetch(`${url_arduino}/api/command`, requestOptions)
+    .then(response => response.json())
+    .then(result => result)
+    .catch(error => console.error(error));
 }
--- app/static/lights/lights.js
+++ app/static/lights/lights.js
@@ -1,5 +1,6 @@
 function refreshValue(strip, values){
-  var values = JSON.parse(values)
+
+  values = values.body
   const leds = document.querySelectorAll(`input[data-strip='${strip}']`)
   for (var i = 0; i < values.length; i++) {
     leds[i].value = values[i]
@@ -7,7 +8,7 @@ function refreshValue(strip, values){
 }
 
 function refreshBrigth(strip, value){
-  var value = JSON.parse(value)
+  values = values.body.bright
   e = document.querySelector(`input[data-strip='${strip}'][data-led='br']`)
   if(e){
       e.value = value[0]
@@ -26,47 +27,37 @@ function setTarget(dataset){
   }
   var n = values.length
   values = values.join('-')
-  return `comm/0-admin-led-setTarget-${strip}-${n}-${values}`
+  return `0-admin-led-setTarget-${strip}-${n}-${values}`
 }
+
 function setBright(e){
   var strip = e.dataset.strip
   var value = e.value
-  return `comm/0-admin-led-setBright-${strip}-${value}`
-}
-function setMS(dataset){
-  var strip = dataset.strip
-  return `comm/0-admin-led-setMS-${strip}`
+  return `0-admin-led-setBright-${strip}-${value}`
 }
 
+function setMS(dataset){
+  var strip = dataset.strip
+  return `0-admin-led-setMS-${strip}`
+}
+
+
 function getValue(strip){
-  var cmd = `comm/0-admin-led-getValue-${strip}`
-  fetch(`${url}/${cmd}`, {method: 'POST'})
-      .then( function(response) {
-        if (response.status !== 200)  return false;
-          response.text().then(function(data) {
-            refreshValue(strip, data)
-          }
-        ).catch(err => console.log("ERROR"))
-      })
-    .catch(err => false);
-} 
+  sendCommand(`0-admin-led-getValue-${strip}`)
+    .then(data => refreshValue(strip, data))
+
+
+
+}
 function getBright(strip){
-  var cmd = `comm/0-admin-led-getBright-${strip}`
-  fetch(`${url}/${cmd}`, {method: 'POST'})
-      .then( function(response) {
-        if (response.status !== 200)  return false;
-          response.text().then(function(data) {
-            refreshBrigth(strip, data)
-          }
-        ).catch(err => console.log(err))
-      })
-    .catch(err => false);
-} 
+  sendCommand(`0-admin-led-getBright-${strip}`)
+    .then(data => refreshValue(strip, data))
+}
 
 function setColor(strip, color){
   var n = color.length
   var values = color.join('-')
-  sendCommand(`comm/0-admin-led-setTarget-${strip}-${n}-${color}`)
+  sendCommand(`0-admin-led-setTarget-${strip}-${n}-${color}`)
   refreshValue(2, JSON.stringify(color))
 }
 
